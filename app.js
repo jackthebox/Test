fcacab9f91dd9f072eb23d8c3850e8886c639e96
jackthebox <jackthebox@gmail.com>
2024-04-10 11:03:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,42 @@
-const express = require("express");
+const express = require('express');
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const Anthropic = require('@anthropic-ai/sdk');
+const dotenv = require('dotenv')
+
 const app = express();
-const port = process.env.PORT || 3001;
+dotenv.config();
+const client = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY});
 
-app.get("/", (req, res) => res.type('html').send(html));
+async function main() {
+  const result = await client.completions.create({
+    promt: "\n\nHuman: Act a helpful AI assistant for a church. ${Anthropic.AI_PROMT}",
+    model: "claude-2.1",
+    max_tokens_to_sample: 300,
+  });
+  console.log(result.completion);
+}
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+// Create a route that will handle Twilio webhook requests, sent as an
+// HTTP POST to /voice in our application
+app.post('/voice', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
+  twiml.say('Hello, Step One is done.');
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+// Create an HTTP server and listen for requests on port 8080
+app.listen(8080, () => {
+  console.log(
+    'Now listening on port 8080. ' +
+    'Be sure to restart when you make code changes!'
+  );
+});
